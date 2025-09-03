1a73264b956ca5e83323c22934e942879d7d6048
wwlight <wwlight8@qq.com>
2025-09-03 13:56:03 +0800

    feat: 借助委派处理动态元素事件绑定

--- app/components/bpmn/_custom-extension/module.js
+++ app/components/bpmn/_custom-extension/module.js
@@ -1,43 +1,85 @@
 import { DBLCLICK_PROPERTY_CONFIG } from '~/constants'
 
 function RegisterDblclickEvent(eventBus, config) {
-  let doubleClickHandlers = {}
+  let isUserTaskSelected = false
+  let currentHandler = null
   const selectors = [
     'input[name=assignee]',
     'input[name=candidateGroups]',
     'input[name=formKey]',
   ]
 
-  eventBus.on('selection.changed', (e) => {
-    const selection = e.newSelection?.[0]
-    if (selection && selection.type === 'bpmn:UserTask') {
-      config.selectionId = selection?.id
+  function handleDoubleClick(type, value) {
+    Object.assign(config, {
+      type,
+      visible: true,
+      value: value || '',
+    })
+  }
 
-      setTimeout(() => {
-        selectors.forEach((selector) => {
-          const inputElement = document.querySelector(selector)
-          if (doubleClickHandlers[selector] && inputElement) {
-            inputElement.removeEventListener('dblclick', doubleClickHandlers[selector])
-          }
+  // 绑定双击事件
+  function bindDoubleClickEvents() {
+    const propertiesPanel = document.getElementById('properties-panel')
+    if (!propertiesPanel)
+      return
 
-          doubleClickHandlers[selector] = (e) => {
-            Object.assign(config, {
-              type: selector.slice(11, -1),
-              visible: true,
-              value: e?.target?.value ?? '',
-            })
-          }
+    if (currentHandler) {
+      unbindDoubleClickEvents()
+    }
 
-          if (inputElement) {
-            inputElement.addEventListener('dblclick', doubleClickHandlers[selector])
-          }
-        })
+    // 创建新的事件处理器
+    currentHandler = (e) => {
+      const target = e.target
+
+      selectors.forEach((selector) => {
+        if (target.matches(selector)) {
+          const type = selector.slice(11, -1)
+          handleDoubleClick(type, target.value)
+        }
       })
     }
+
+    propertiesPanel.addEventListener('dblclick', currentHandler)
+  }
+
+  function unbindDoubleClickEvents() {
+    const propertiesPanel = document.getElementById('properties-panel')
+    if (propertiesPanel && currentHandler) {
+      propertiesPanel.removeEventListener('dblclick', currentHandler)
+      currentHandler = null
+    }
+  }
+
+  eventBus.on('selection.changed', (e) => {
+    const selection = e.newSelection?.[0]
+
+    if (selection && selection.type === 'bpmn:UserTask') {
+      // 如果之前已经选中了 UserTask，不需要重新绑定
+      if (!isUserTaskSelected) {
+        config.selectionId = selection?.id
+        isUserTaskSelected = true
+
+        setTimeout(() => {
+          bindDoubleClickEvents()
+        })
+      }
+    }
     else {
-      Object.assign(config, DBLCLICK_PROPERTY_CONFIG)
+      if (isUserTaskSelected) {
+        isUserTaskSelected = false
+        unbindDoubleClickEvents()
+        Object.assign(config, DBLCLICK_PROPERTY_CONFIG)
+      }
     }
   })
+
+  function cleanup() {
+    unbindDoubleClickEvents()
+    isUserTaskSelected = false
+  }
+
+  eventBus.on('diagram.destroy', cleanup)
+  window.addEventListener('beforeunload', cleanup)
 }
 
 RegisterDblclickEvent.$inject = ['eventBus']
